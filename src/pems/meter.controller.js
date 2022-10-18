1796f15fa5a15e983e03b2293af29b95d0eed294
Fred Guo <fred.guo@rockwellautomation.com>
2022-10-18 19:22:28 +0800

    Add Meter Pagination Controller

--- src/pems/meter.controller.js
+++ src/pems/meter.controller.js
@@ -70,6 +70,113 @@ const controller = (() => {
     }
   });
 
+  /**
+   * @swagger
+   * /api/pems/meter/count:
+   *   get:
+   *     security:
+   *       - Authorization: []
+   *     description: Get count for meters!
+   *     tags: [pems]
+   *     responses:
+   *       200:
+   *         description: Returns a mysterious string.
+   */
+  router.get('/count', async (req, res) => {
+    const data = await prisma.Pems_Meter.count();
+    res.json({ data, message: 'Data obtained.' });
+  });
+
+  /**
+   * @name pagination - get a list of paging
+   * @param {number} [page=1] - current page number
+   * @param {number} [row=5] - rows per page
+   * @return {Object<{ data: ListColl[], message: string }>}
+   *
+   * @example GET /crud-operations/pagination?page=${page}&row=${row}
+   */
+
+  /**
+   * @swagger
+   * /api/pems/meterPosition/pagination:
+   *   get:
+   *     security:
+   *       - Authorization: []
+   *     description: Login to the application
+   *     tags: [pems]
+   *     produces:
+   *       - application/json
+   *     parameters:
+   *       - name: page
+   *         description: pagination's pageindex.
+   *         in: query
+   *         required: true
+   *         type: number
+   *       - name: row
+   *         description: pagination's row count.
+   *         in: query
+   *         required: true
+   *         type: number
+   *     responses:
+   *       200:
+   *         description: meterPosition/pagination
+   *         schema:
+   *           type: object
+   */
+  router.get('/pagination', async (req, res) => {
+
+    const { cPositionFk } = req.query;
+
+    const filter = { AND: {} };
+
+    if (cPositionFk) filter.AND = { ...filter.AND, cPositionFk: Number(cPositionFk) };
+
+    const page = Number(req.query.page) || 1;
+    const row = Number(req.query.row) || 5;
+    const count = await prisma.Pems_Meter.count({
+      where: filter,
+    });
+
+    const select = {
+      id: true,
+      cName: true,
+      cType: true,
+      cDesc: true,
+      cPositionFk: true,
+      dAddTime: true,
+      Pems_MeterPosition: {
+        select: {
+          id: true,
+          cName: true,
+          dAddTime: true,
+        },
+      },
+    };
+
+    if (count != null && count > 0) {
+      const rstdata = await prisma.Pems_Meter.findMany({
+        select,
+        skip: (page - 1) * row,
+        take: row,
+        orderBy: {
+          id: 'asc',
+        },
+      });
+
+      res.json({
+        data: rstdata,
+        total: count,
+        message: 'Data obtained.',
+      });
+    } else {
+      res.json({
+        data: [],
+        total: count,
+        message: 'Data Empty.',
+      });
+    }
+  });
+
   return router;
 })();
 
--- src/schedule/service.js
+++ src/schedule/service.js
@@ -1,8 +1,27 @@
 const CronJob = require('node-cron');
 import prisma from '../core/prisma';
 
+const getMeterPositions = async () => {
+  const select = {
+    id: true,
+    cName: true,
+    parentId: true,
+    dAddTime: true,
+    Pems_Meter: {
+      select: {
+        cName: true,
+        cType: true,
+        cDesc: true,
+        dAddTime: true,
+      },
+    },
+  };
+  const data = await prisma.Pems_MeterPosition.findMany({ select });
+  console.log(data);
+};
+
 exports.initScheduledJobs = () => {
-  const scheduledJobFunction = CronJob.schedule('*/1 * * * *', async res => {
+  const scheduledJobFunction = CronJob.schedule('*/1 * * * *', res => {
     console.log("I'm executed on a schedule!");
     // Add your custom logic here
   });
