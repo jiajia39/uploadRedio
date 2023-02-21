41bcd54bf93e3d6fda7277285ccdf7e4d5341b11
jiajia <2384924339@qq.com>
2023-02-21 19:08:37 +0800

    addinterface - save energy consumption excel of last month

--- src/pems/report.service.js
+++ src/pems/report.service.js
@@ -1,6 +1,6 @@
 import xlsx from 'node-xlsx';
+import energyService from './energy.service';
 import prisma from '../core/prisma';
-
 const moment = require('moment');
 const fs = require('fs');
 
@@ -46,7 +46,7 @@ order by  meter.cType asc  `;
   const end = `${nowFor} 读数`;
   const sta = `${preFor} 读数`;
   let dataList = [];
-  const title = ['TagName', 'DataType', 'Description', sta, end, '差值'];
+  const title = ['名称', '类型', '描述', sta, end, '差值'];
   dataList.push(title);
   data.forEach(element => {
     let arrinner = [];
@@ -60,7 +60,347 @@ order by  meter.cType asc  `;
   });
   writexls(dataList);
 }
+async function getMonEnergyConsumption() {
+  const preDay = moment()
+    .date(1)
+    .format('YYYY-MM-DD');
+
+  const prePreDay = moment(preDay)
+    .subtract(1, 'months')
+    .date(1)
+    .format('YYYY-MM-DD');
+  const predate = moment(prePreDay)
+    .date(1)
+    .format('YYYY-MM');
+  const data = await prisma.$queryRaw` exec [dbo].[report_Month_Energy_Consumption] ${prePreDay},${preDay}`;
+  const dataList = [];
+  dataList.push(['每月抄表记录(每月抄一次)']);
+  dataList.push([predate]);
+  let prePre = prePreDay + '表数';
+  let pre = preDay + '表数';
+
+  let preMon = moment(predate).month();
+  let diffVal = Number(preMon) + 1 + '月用电数';
+  const title = ['序号', '抄表地点', '位置', '名称', '倍率', prePre, pre, diffVal];
+  dataList.push(title);
+  let ind = 0;
+  data.forEach(element => {
+    const arrinner = [];
+    ind += 1;
+    arrinner.push(ind);
+    arrinner.push(element.postionName);
+    arrinner.push(element.cDesc);
+    arrinner.push(element.cName);
+    arrinner.push(element.multiplier);
+    arrinner.push(element[prePreDay]);
+    arrinner.push(element[preDay]);
+    let diff = null;
+    if (element[prePreDay] != null && element[preDay] != null) {
+      diff = Number(element[preDay]) - Number(element[prePreDay]);
+    }
+    arrinner.push(diff);
+    dataList.push(arrinner);
+  });
+  return { dataList, data };
+}
+
+/**
+ * 获取上一个月的报表数据生成excel并保存到对应的位置
+ */
+async function monthExportExcel() {
+  //获取上月的第一天和最后一天
+  const firstDay = moment()
+    .subtract(1, 'months')
+    .date(1)
+    .format('YYYY-MM-DD');
+  const DateMon = moment()
+    .subtract(1, 'months')
+    .date(1)
+    .format('YYYY-MM');
+  const endDay = moment(firstDay)
+    .endOf('month')
+    .format('YYYY-MM-DD');
+  const month = moment(firstDay).month() + 1;
+  const year = moment(firstDay).year();
+  const yearMonth = year + '年' + month + '月';
+  const days = await energyService.getAllDays(firstDay, endDay);
+  let dayStr = '';
+  days.forEach(element => {
+    if (days[days.length - 1] == element) {
+      dayStr += '[' + element + ']';
+    } else {
+      dayStr += '[' + element + ']' + ',';
+    }
+  });
+  const data = await prisma.$queryRaw` exec [dbo].[report_Month] ${firstDay},${endDay}
+  `;
+
+  const dataList = [];
+  // dataList.push([yearMonth]);
+  const title = [
+    '名称',
+    '描述',
+    '01',
+    '02',
+    '03',
+    '04',
+    '05',
+    '06',
+    '07',
+    '08',
+    '09',
+    '10',
+    '11',
+    '12',
+    '13',
+    '14',
+    '15',
+    '16',
+    '17',
+    '18',
+    '19',
+    '20',
+    '21',
+    '22',
+    '23',
+    '24',
+    '25',
+    '26',
+    '27',
+    '28',
+    '29',
+    '30',
+    '31',
+    'Total',
+  ];
+  dataList.push([DateMon]);
+  dataList.push(title);
+  let sumAll = 0;
+  data.forEach(element => {
+    const arrinner = [];
+    arrinner.push(element.cName);
+    arrinner.push(element.cDesc);
+    let value = 0;
+    days.forEach(day => {
+      if (element[day] == undefined) {
+        element[day] = null;
+      } else {
+        value = value + element[day];
+      }
+      arrinner.push(element[day]);
+    });
+    arrinner.push(value);
+    sumAll = sumAll + value;
+    dataList.push(arrinner);
+  });
+  const dateSum = await prisma.$queryRaw`select day.cDate as ccDate, sum(cValue) as sumValue
+   from Pems_Meter meter
+            left join Pems_MeterReportHistory_Day day on day.cMeterFk = meter.id
+    where cDate >= ${firstDay}
+     and cDate <= ${endDay}
+   group by day.cDate order by  day.cDate`;
+  const arr = [];
+  arr.push(null);
+  arr.push('每日用量 Total');
+  days.forEach(day => {
+    let num = 0;
+    dateSum.forEach(sum => {
+      const date1 = moment(sum.ccDate).format('YYYY-MM-DD');
+      if (date1.toString() == day.toString()) {
+        arr.push(sum.sumValue);
+        num = 1;
+      }
+    });
+    if (num == 0) {
+      arr.push(null);
+    }
+  });
+  arr.push(sumAll);
+  dataList.push(arr);
+  // console.log(dataList[1]);
+  return dataList;
+}
+/**
+ * 保存每月耗能数据excel
+ */
+async function saveExcel() {
+  var Excel = require('exceljs');
+  var workbook = new Excel.Workbook();
+  // 标签创建
+  var worksheet = workbook.addWorksheet('上月仪器每日用电量');
+  // 带颜色的
+  var worksheet2 = workbook.addWorksheet('每月抄表记录(每月抄一次)', {
+    properties: {
+      tabColor: {
+        argb: 'FFC0000',
+      },
+    },
+  });
+  let data = await this.monthExportExcel();
+
+  worksheet.addRows(data);
+  // ===== 字体显示
+  worksheet.getCell('A1').font = {
+    // 字体名
+    name: 'Comic Sans MS',
+    // Font family for fallback. An integer value.
+    family: 4,
+    // 字体大小
+    size: 16,
+    // // 下划线
+    // underline: true,
+    // 加粗
+    bold: true,
+  };
+
+  worksheet.getRow('2').font = {
+    // 字体大小
+    size: 14,
+  };
+  // worksheet.getCell('A3').value = '测试字体';
+
+  // ==== 对齐方式
+  worksheet.getCell('A1').alignment = {
+    vertical: 'top',
+    horizontal: 'left',
+  };
+  worksheet.getCell('B1').alignment = {
+    vertical: 'middle',
+    horizontal: 'center',
+  };
+  worksheet.getCell('C1').alignment = {
+    vertical: 'bottom',
+    horizontal: 'right',
+  };
+  worksheet.getColumn(1).width = 12;
+  worksheet.getColumn(2).width = 70;
+  // ===== 边框
+  worksheet.getRow('2').border = {
+    top: {
+      style: 'double',
+      // color: {
+      //   argb: 'FF00FF00',
+      // },
+    },
+    left: {
+      style: 'thin',
+    },
+    bottom: {
+      style: 'thin',
+    },
+    right: {
+      style: 'thin',
+    },
+  };
+
+  //worksheet2
+
+  let data2 = await this.getMonEnergyConsumption();
+  worksheet2.addRows(data2.dataList);
+  // // 合并单元格
+  const dateInfo = data2.data;
+  if (dateInfo != null && dateInfo.length > 0) {
+    let name = dateInfo[0].postionName;
+    let index = 4;
+    let sum = 4;
+    for (let i = 1; i < dateInfo.length; i++) {
+      if (name == dateInfo[i].postionName) {
+        if (i == dateInfo.length - 1) {
+          name = dateInfo[i - 1].postionName;
+          sum = sum + 1;
+          const col = 'B' + index + ':B' + sum;
+          worksheet2.mergeCells(col);
+          continue;
+        }
+        sum = sum + 1;
+      } else {
+        name = dateInfo[i].postionName;
+        const col = 'B' + index + ':B' + sum;
+        console.log(col);
+        worksheet2.mergeCells(col);
+        index = sum + 1;
+        sum = sum + 1;
+      }
+    }
+  }
+  //列宽
+  worksheet2.getColumn(2).width = 20;
+  worksheet2.getColumn(3).width = 70;
+  worksheet2.getColumn(4).width = 16;
+  worksheet2.getColumn(6).width = 20;
+  worksheet2.getColumn(7).width = 20;
+  worksheet2.getColumn(8).width = 18;
+  //行高
+  worksheet2.getRow(3).height = 25;
+  worksheet2.getColumn('B').alignment = {
+    vertical: 'middle',
+    horizontal: 'center',
+  };
+  worksheet2.getCell('A1').font = {
+    // 字体名
+    name: 'Comic Sans MS',
+    // Font family for fallback. An integer value.
+    family: 4,
+    // 字体大小
+    size: 16,
+    // // 下划线
+    // underline: true,
+    // 加粗
+    bold: true,
+  };
+  worksheet2.getCell('A2').font = {
+    // Font family for fallback. An integer value.
+    family: 4,
+    // 字体大小
+    size: 10,
+    // // 下划线
+    // underline: true,
+    // 加粗
+    bold: true,
+  };
+  worksheet2.getRow('3').font = {
+    // Font family for fallback. An integer value.
+    family: 4,
+    // 字体大小
+    size: 14,
+    // // 下划线
+    // underline: true,
+    // 加粗
+    bold: true,
+  };
+  // ===== 边框
+  worksheet2.getRow('3').border = {
+    top: {
+      style: 'double',
+      // color: {
+      //   argb: 'FF00FF00',
+      // },
+    },
+    left: {
+      style: 'thin',
+    },
+    bottom: {
+      style: 'thin',
+    },
+    right: {
+      style: 'thin',
+    },
+  };
+  const moment = require('moment');
+  // 直接创建一个Excel表
+  const nowDate = moment()
+    .subtract(1, 'month')
+    .format('YYYY-MM');
+  const path = './uploads/' + '读数统计' + `${nowDate}.xlsx`;
+  workbook.xlsx.writeFile(path).then(function() {
+    // console.log('saved');
+  });
+}
 
 export default {
   exportExcel,
+  monthExportExcel,
+  writexls,
+  saveExcel,
+  getMonEnergyConsumption,
 };
