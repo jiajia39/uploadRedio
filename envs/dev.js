9594ea6995a281702227aa3fc1610d13682a64d2
JIAJIAGAO\gj238 <2384924339@qq.com>
2023-03-09 14:24:31 +0800

    alter Influx Token

--- envs/dev.js
+++ envs/dev.js
@@ -1,19 +1,18 @@
 function Environment() {
-
   this.NODE_ENV = 'development';
 
   this.HOST = process.env.HOST || '0.0.0.0';
-  
+
   this.PORT = 3000;
 
   this.INFLUX_URL = 'https://iad.raopc.com:7086/';
 
-  this.INFLUX_TOKEN = 'kVAfPlHgzR-ssVptzXffkej9cBxxge4zCOOW7aGl9eCVniMlbnyyctMnnvHKMYLxdPVKL9tBMp3V-CbLyrid1g==';
+  this.INFLUX_TOKEN =
+    'qk4OpqZDAlEQyghsz1Ef6Xw-bPvXsb1P1eCfpv83vtjVXe7zmlMYkTXmDLzMipzldY6wTnyoG4MMAQyAiWMOXg==';
 
   this.INFLUX_ORG = 'trz';
 
   this.INFLUX_BUCKET = 'trz_pems';
-  
 }
 
 module.exports = new Environment();
