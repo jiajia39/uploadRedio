7d8192d8906d14d81189aa34c941042f39cc964f
JIAJIAGAO\gj238 <2384924339@qq.com>
2023-04-10 09:56:05 +0800

    bug fix for influx config

--- envs/prod.js
+++ envs/prod.js
@@ -1,19 +1,18 @@
 function Environment() {
-
   this.NODE_ENV = 'production';
 
   this.HOST = process.env.HOST || '0.0.0.0';
-  
+
   this.PORT = 3000;
 
   this.INFLUX_URL = 'http://10.232.229.155:8086/';
 
-  this.INFLUX_TOKEN = 'Dwu2vyFmBTOFSGF1VF7JkDteY554MxPpw7euRhcA4x8snb1gAkDbfA8hk32HNLZbss_UZS7xcdF1octkmBtCnQ==';
+  this.INFLUX_TOKEN =
+    'Dwu2vyFmBTOFSGF1VF7JkDteY554MxPpw7euRhcA4x8snb1gAkDbfA8hk32HNLZbss_UZS7xcdF1octkmBtCnQ==';
 
   this.INFLUX_ORG = 'trz';
 
-  this.INFLUX_BUCKET = 'trz_pems_proc';
-  
+  this.INFLUX_BUCKET = 'trz_pems_prod';
 }
 
 module.exports = new Environment();
